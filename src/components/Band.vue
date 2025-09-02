10e8258965da57955321c94375ffe78b3ff372db
abloom25 <evereen2023@gmail.com>
2025-09-02 09:21:25 +0900

    fix: layout problem

---
+++ src/components/Band.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="band">
+        <p class="band-label">
+            Site Under Construction. For any development issues please contact us at
+            <a href="mailto:makabaka1880@outlook.com">makabaka1880@outlook.com</a>
+            or state an issue at
+            <a href="https://github.com/neur0craft/neur0craft.github.io/issues">our github repository</a>.
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
